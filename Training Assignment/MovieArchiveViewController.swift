b989fdb7c737508fa67922a1d0578c65c4972147
Ayush Kumar Sinha <ayush.sinha@housing.com>
2023-08-23 10:50:36 +0530

    Adding the Views for Movies Archive and Details View(in Storyboard) and their Controller files

---
+++ Training Assignment/MovieArchiveViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieArchiveViewController.swift
+//  Training Assignment
+//
+//  Created by Ayush Kumar Sinha on 23/08/23.
+//
+
+import UIKit
+
+class MovieArchiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
