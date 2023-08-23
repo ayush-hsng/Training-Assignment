388fba459c0472815a535e67666568f8810920e5
Ayush Kumar Sinha <ayush.sinha@housing.com>
2023-08-23 11:03:21 +0530

    Adding Custom Cell's View and Controller for listing the movies in table view

---
+++ Training Assignment/MovieArchiveTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieArchiveTableViewCell.swift
+//  Training Assignment
+//
+//  Created by Ayush Kumar Sinha on 23/08/23.
+//
+
+import UIKit
+
+class MovieArchiveTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
